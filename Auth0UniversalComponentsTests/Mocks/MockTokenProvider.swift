5dbf116c1bdef0cf10ca956c1ee43fee328404bb
Sudhanshu Vohra <sudhanshu.vohra@okta.com>
2026-04-22 13:39:38 +0530

    Failing test case fixed

--- Auth0UniversalComponentsTests/Mocks/MockTokenProvider.swift
+++ Auth0UniversalComponentsTests/Mocks/MockTokenProvider.swift
@@ -10,7 +10,7 @@ struct MockTokenProvider: TokenProvider {
 
     func storeCredentials(credentials: Credentials) {}
 
-    func store(apiCredentials: APICredentials, for audience: String) {}
+    func store(apiCredentials: APICredentials, for audience: String, andScope scope: String) {}
 
     func fetchAPICredentials(audience: String, scope: String) async throws -> APICredentials {
         APICredentials(accessToken: "mock-access-token", tokenType: "Bearer", expiresIn: Date(), scope: "openid profile offline_access")
