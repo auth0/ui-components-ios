5c167cfe8d3d71083e5830d9d8ab519b34923c43
Sudhanshu Vohra <sudhanshu.vohra@okta.com>
2026-08-11 12:57:53 +0530

    Removed unused code

--- scripts/utils/helpers.mjs
+++ scripts/utils/helpers.mjs
@@ -1,5 +1,4 @@
 import readline from "node:readline/promises"
-import { select } from "@inquirer/prompts"
 
 /**
  * Wait for user confirmation before proceeding
@@ -14,27 +13,4 @@ export async function confirmWithUser(message) {
   rl.close()
 
   return answer.toLowerCase() === "y" || answer.toLowerCase() === "yes"
-}
-
-/**
- * Wait for user input
- */
-export async function getInputFromUser(message) {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  })
-
-  const answer = await rl.question(`${message} `)
-  rl.close()
-
-  return answer.toLowerCase()
-}
-
-/**
- * Prompt the user to select one option from a list
- */
-export async function selectOptionFromList(message, options) {
-  const answer = await select({ message: message, choices: options })
-  return answer
 }
